aac3278362c88e58d5b585e5815fb28ee8a0a04f
Muzamil Mohammad <muzamil.mohammad7@homeoffice.gsi.gov.uk>
2018-06-29 10:23:37 +0100

    first eclipse file

---
+++ src/com/Junit/Muzi.java
@@ -0,0 +1,11 @@
+package com.Junit;
+
+public class Muzi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("helloo muzz");
+	}
+
+}
---
+++ src/com/Junit/muzi1.java
@@ -0,0 +1,10 @@
+package com.Junit;
+
+public class muzi1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
